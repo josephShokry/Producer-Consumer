24977410f23181b0173f57f760eaa22297fb906d
OmarTammam25 <omartammam25@gmail.com>
2023-01-27 18:24:37 +0200

    change color randomization

--- src/main/java/com/producer_consumer/models/Product.java
+++ src/main/java/com/producer_consumer/models/Product.java
@@ -10,8 +10,8 @@ public class Product {
     private static String[] colorss = {"red","green","yellow","pink","blue","#ffba00"};
     public Product() {
         int min = 0;
-        int max = colorss.length;
-        int randomInd = (int)Math.floor(Math.random() *(max - min + 1) + min);
+        int max = colorss.length-1;
+        int randomInd = (int)Math.floor(Math.random() *(max - min + 1) + min) % 6;
         this.color = colorss[randomInd];
     }
 }
